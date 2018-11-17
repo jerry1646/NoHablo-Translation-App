e4243013845c533af40b921c79f477502dd2df5d
jerry1646 <jerrysong131@gmail.com>
2018-11-17 17:17:19 +0000

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Users from "./components/users/users";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React App</h1>
+        <Users/>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/api/users', (req, res) => {
+  const users = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Steve', lastName: 'Stan'},
+    {id: 3, firstName: 'Marrie', lastName: 'Swanson'},
+  ]
+  res.json(users)
+})
+
+
+
+const port = 5000
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+})
